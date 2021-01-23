e8678f205f9c9a8ff01c2db762cd413f9f189562
Hans Spaans <hans@dailystuff.nl>
2021-01-23 23:49:56 +0100

    Delete install-wpcli.sh

--- install-wpcli.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-curl -o /home/vscode/bin/wp https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-
-chown -R vscode:vscode /home/vscode/bin
-chmod 755 /home/vscode/bin/wp
